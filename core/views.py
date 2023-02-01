f394e7d0e9a6b804fb31eec517df58178a055638
Kostadin Stanev <stanebvns@gmail.com>
2023-02-01 11:24:31 +0200

    initialized about us page

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def contacts(request):
     return render(request, template_name='contacts.html')
+
+
+def about_us(request):
+    return render(request, template_name='about.html')
