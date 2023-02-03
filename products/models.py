feaec9120670fcb47c870323543b0cc9a47adff1
Kostadin Stanev <stanebvns@gmail.com>
2023-02-03 14:03:00 +0200

    products django model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        blank=False,
+        null=False,
+        max_length=60,
+    )
+
+    price = models.PositiveIntegerField(
+        blank=False,
+        null=False,
+    )
+
+    description = models.CharField(
+        blank=True,
+        null=True,
+        max_length=120,
+    )
+
+    image = models.ImageField(
+        default='defaults/default_product.png',
+        upload_to='product_photos',
+        blank=True,
+        null=True,
+        verbose_name='Product Photos',
+    )
+
+    def __str__(self):
+        return f'{self.name} - {self.price} USD'
